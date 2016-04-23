80d1e5f703c68444e19758cfe695cf10b66e4fb9
Colin McKenna <colinjmckenna@gmail.com>
2016-04-23 16:46:24 -0700

    initialize repo

---
+++ bubbleSort.py
@@ -0,0 +1,23 @@
+import random
+
+def bubble_sort(list):
+  ending_index = len(list)-1
+  while ending_index > 0:
+    for index in range(0, ending_index):
+      if list[index] > list[index +1]:
+        temp = list[index]
+        list[index] = list[index+1]
+        list[index+1] = temp
+    ending_index -= 1
+  print list
+
+def random_list():
+  newList = []
+  for times in range(0, 100):
+    number = random.randint(1, 10000)
+    newList.append(number)
+  return newList
+
+numbers = random_list()
+bubble_sort(numbers)
+
